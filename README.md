723b5acf3d2c3aabdcedb852ebbcb3db230d3554
Isaac B. Conteh <67395857+Zacflixx@users.noreply.github.com>
2021-03-05 22:36:12 +0800

    merged z journal auth

---
+++ README.md
@@ -0,0 +1,36 @@
+# React Native Firebase Social App
+
+This repository is based on my YouTube tutorial series where I'm creating a react native social app with Firebase.
+
+## Watch it on YouTube
+- [Onboarding UI in React Native](https://www.youtube.com/watch?v=SMkR-iIGvwQ)
+- [Login & Signup UI in React Native](https://www.youtube.com/watch?v=ZxP-0xbz5sg)
+- [Firebase Authentication in React Native](https://youtu.be/J7pkSP18Oko)
+- [Google Login in React Native with Firebase](https://youtu.be/SdYp5JdMvs0)
+- [Facebook Login in React Native with Firebase](https://youtu.be/pDLo7Pfcvfk)
+- [Home/Feed Screen UI of Social App using Styled-Components](https://youtu.be/iyNmGXt4vNA)
+- [React Native Firebase Storage Tutorial for Storing Photos](https://youtu.be/1GpOS5mrGHI)
+- [React Native Firebase Firestore Tutorial | Create/Fetch/Delete Posts](https://youtu.be/ncxmNxNk2yc)
+- [Image Loader & Shimmer Effect for Posts on Home Screen](https://youtu.be/bHWoW_aNmnY)
+- [Messages & Chat Screen UI](https://youtu.be/bGGeD5RkdzQ)
+- [User Profile with Firebase](https://youtu.be/aFtYsghw-1k)
+
+## Packages Used
+- [React Native Onboarding Swiper](https://github.com/jfilter/react-native-onboarding-swiper)
+- [React Navigation v5](https://reactnavigation.org/)
+- [React Native Firebase v6](https://rnfirebase.io/)
+- [React Native Vector Icons](https://github.com/oblador/react-native-vector-icons)
+- [Styled Components](https://styled-components.com/)
+- [React Native Action Button](https://github.com/mastermoo/react-native-action-button)
+- [React Native Image Crop Picker](https://github.com/ivpusic/react-native-image-crop-picker)
+- [React Native Skeleton Placeholder](https://github.com/chramos/react-native-skeleton-placeholder)
+- [React Native Gifted Chat](https://github.com/FaridSafi/react-native-gifted-chat)
+
+### Onboarding UI
+![Onboarding UI](https://raw.githubusercontent.com/itzpradip/react-native-firebase-social-app/master/assets/screenshots/onboarding-ui.png)
+
+### Sign In UI
+![Sign in UI](https://raw.githubusercontent.com/itzpradip/react-native-firebase-social-app/master/assets/screenshots/social-login.png)
+
+### Home/Feed Screen UI
+![Home/Feed Screen UI](https://raw.githubusercontent.com/itzpradip/react-native-firebase-social-app/master/assets/screenshots/social-app-ui.png)
---
+++ screens/LoginScreen.js
@@ -0,0 +1,287 @@
+import React, {useContext, useState} from 'react';
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  TextInput,
+  Platform,
+  StyleSheet,
+  StatusBar,
+  Image,
+  ScrollView,
+  Alert,
+} from 'react-native';
+import * as Animatable from 'react-native-animatable';
+import LinearGradient from 'react-native-linear-gradient';
+import FontAwesome from 'react-native-vector-icons/FontAwesome';
+import Feather from 'react-native-vector-icons/Feather';
+
+import {AuthContext} from '../navigation/AuthProvider.android';
+
+// import Users from '../model/users';
+
+const SignInScreen = ({navigation}) => {
+  const [data, setData] = React.useState({
+    // username: '',
+    // password: '',
+    // check_textInputChange: false,
+    secureTextEntry: true,
+    // isValidUser: true,
+    // isValidPassword: true,
+  });
+
+  const [email, setEmail] = useState();
+  const [password, setPassword] = useState();
+
+  const {login, googleLogin, fbLogin} = useContext(AuthContext);
+
+  // const {colors} = useTheme();
+
+  const loginHandle = (userName, password) => {
+    const foundUser = Users.filter((item) => {
+      return userName == item.username && password == item.password;
+    });
+
+    if (data.username.length == 0 || data.password.length == 0) {
+      Alert.alert(
+        'Wrong Input!',
+        'Username or password field cannot be empty.',
+        [{text: 'Okay'}],
+      );
+      return;
+    }
+
+    if (foundUser.length == 0) {
+      Alert.alert('Invalid User!', 'Username or password is incorrect.', [
+        {text: 'Okay'},
+      ]);
+      return;
+    }
+    signIn(foundUser);
+  };
+
+  return (
+    <View style={styles.container}>
+      <StatusBar backgroundColor="#c4ae66" barStyle="light-content" />
+      <View style={styles.header}>
+        <Text style={styles.text_header}>Welcome </Text>
+      </View>
+      <View>
+        <Text style={styles.text_header2}>Please Sign In To Continue </Text>
+      </View>
+      <Animatable.View
+        animation="fadeInUpBig"
+        style={[
+          styles.footer,
+          {
+            backgroundColor: '#0d0d05',
+          },
+        ]}>
+        <Text
+          style={[
+            styles.text_footer,
+            {
+              color: '#c4ae66',
+            },
+          ]}>
+          Username
+        </Text>
+        <View style={styles.action}>
+          <FontAwesome name="user-o" color="#c4ae66" size={20} />
+          <TextInput
+            placeholder="Your Username"
+            placeholderTextColor="#666666"
+            style={styles.textInput}
+            labelValue={email}
+            onChangeText={(userEmail) => setEmail(userEmail)}
+            keyboardType="email-address"
+            autoCapitalize="none"
+            autoCorrect={false}
+          />
+          {data.check_textInputChange ? (
+            <Animatable.View animation="bounceIn">
+              <Feather name="check-circle" color="#c4ae66" size={20} />
+            </Animatable.View>
+          ) : null}
+        </View>
+        {/* {data.isValidUser ? null : (
+          <Animatable.View animation="fadeInLeft" duration={500}>
+            <Text style={styles.errorMsg}>
+              Username must be 4 characters long.
+            </Text>
+          </Animatable.View>
+        )} */}
+
+        <Text
+          style={[
+            styles.text_footer,
+            {
+              color: '#c4ae66',
+
+              marginTop: 35,
+            },
+          ]}>
+          Password
+        </Text>
+        <View style={styles.action}>
+          <Feather name="lock" color="#c4ae66" size={20} />
+          <TextInput
+            placeholder="Your Password"
+            placeholderTextColor="#666666"
+            style={[
+              styles.textInput,
+              {
+                color: '#c4ae66',
+              },
+            ]}
+            autoCapitalize="none"
+            labelValue={password}
+            onChangeText={(userPassword) => setPassword(userPassword)}
+            placeholderText="Password"
+            iconType="lock"
+            secureTextEntry={true}
+          />
+          {/* <TouchableOpacity onPress={updateSecureTextEntry}>
+            {data.secureTextEntry ? (
+              <Feather name="eye-off" color="#c4ae66" size={20} />
+            ) : (
+              <Feather name="eye" color="#c4ae66" size={20} />
+            )}
+          </TouchableOpacity> */}
+        </View>
+        {/* {data.isValidPassword ? null : (
+          <Animatable.View animation="fadeInLeft" duration={500}>
+            <Text style={styles.errorMsg}>
+              Password must be 8 characters long.
+            </Text>
+          </Animatable.View>
+        )} */}
+
+        <TouchableOpacity>
+          <Text style={{color: '#c4ae66', marginTop: 15}}>
+            Forgot password?
+          </Text>
+        </TouchableOpacity>
+        <View style={styles.button}>
+          <TouchableOpacity
+            onPress={() => login(email, password)}
+            style={[
+              styles.signIn,
+              {
+                borderColor: '#c4ae66',
+                borderWidth: 1,
+                marginTop: 15,
+              },
+            ]}>
+            <Text
+              style={[
+                styles.textSign,
+                {
+                  color: '#c4ae66',
+                },
+              ]}>
+              Sign In
+            </Text>
+          </TouchableOpacity>
+
+          <TouchableOpacity
+            style={styles.signIn}
+            onPress={() => navigation.navigate('SignUpScreen')}>
+            <LinearGradient colors={['black', '#c4ae66']} style={styles.signIn}>
+              <Text
+                style={[
+                  styles.textSign,
+                  {
+                    color: 'black',
+                  },
+                ]}>
+                Sign Up
+              </Text>
+            </LinearGradient>
+          </TouchableOpacity>
+        </View>
+      </Animatable.View>
+    </View>
+  );
+};
+
+export default SignInScreen;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#c4ae66',
+  },
+  header: {
+    flex: 1,
+    justifyContent: 'flex-end',
+    paddingHorizontal: 20,
+    paddingBottom: 50,
+  },
+  footer: {
+    flex: 3,
+    backgroundColor: '#fff',
+    borderTopLeftRadius: 50,
+    borderTopRightRadius: 50,
+    paddingHorizontal: 30,
+    paddingVertical: 30,
+  },
+  text_header: {
+    color: 'black',
+    fontWeight: 'bold',
+    fontSize: 40,
+    // textAlign: 'center',
+  },
+  text_header2: {
+    color: 'black',
+    fontWeight: 'bold',
+    fontSize: 14,
+    textAlign: 'center',
+    // marginLeft: 40,
+    marginBottom: 4,
+  },
+  text_footer: {
+    color: '#fff',
+    fontSize: 18,
+  },
+  action: {
+    flexDirection: 'row',
+    marginTop: 10,
+    borderBottomWidth: 1,
+    borderBottomColor: '#c4ae66',
+    paddingBottom: 5,
+  },
+  actionError: {
+    flexDirection: 'row',
+    marginTop: 10,
+    borderBottomWidth: 1,
+    borderBottomColor: '#FF0000',
+    paddingBottom: 5,
+  },
+  textInput: {
+    flex: 1,
+    marginTop: Platform.OS === 'ios' ? 0 : -12,
+    paddingLeft: 10,
+    color: '#05375a',
+  },
+  errorMsg: {
+    color: '#4d3900',
+    fontSize: 14,
+  },
+  button: {
+    alignItems: 'center',
+    marginTop: 50,
+  },
+  signIn: {
+    width: '100%',
+    height: 50,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 10,
+    marginTop: 8,
+  },
+  textSign: {
+    fontSize: 18,
+    fontWeight: 'bold',
+  },
+});
